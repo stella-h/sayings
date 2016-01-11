39b5c8cc2b67848b4da8947058e6dcc24acae3f1
stella-h <stella.halena@gmail.com>
2016-01-12 00:57:45 +1100

    random returning undefined, elementID toa  variable

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 var quotes = ["amazing", "hello", "crap", "oh man", "hey hey"]
+var box = document.getElementById("quote-box")
 
  function generateQuotes() {
   var randomQuotes = quotes[Math.floor(Math.random()*quotes.length++)];
-  document.getElementById("quote-box").value = document.createTextNode(randomQuotes);
   console.log(randomQuotes);
 }
+
+// box.appendChild(generateQuotes);
