daef477b8d7cfb0d74ed1fdbf7759dd8ab1e99fb
stella-h <stella.halena@gmail.com>
2016-01-19 00:32:48 +1100

    array works without showing negative values. WIN

--- script/script.js
+++ script/script.js
@@ -2,8 +2,9 @@ var quotes = ["amazing", "hello", "crap", "oh man", "hey hey"]
 var box = document.getElementById("quote-box")
 
  function generateQuotes() {
-  var randomQuotes = quotes[Math.floor(Math.random()*quotes.length++)];
+  var randomQuotes = quotes[Math.floor(Math.random()*quotes.length)];
   console.log(randomQuotes);
 }
+
 
 // box.appendChild(generateQuotes);
