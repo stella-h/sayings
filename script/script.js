b2835ff6d0289ef691110188142f45e72d949658
stella-h <stella.halena@gmail.com>
2016-01-24 04:26:10 +1100

    rubbish

--- script/script.js
+++ script/script.js
@@ -1,10 +1,40 @@
-var quotes = ["amazing", "hello", "crap", "oh man", "hey hey"]
-var box = document.getElementById("quote-box")
+
+var quotes = 
+{ key1:"amazing",
+  key2: "hello", 
+  key3:"crap",
+  key4:"oh man",
+  key5:"hey hey" }
+
+// max = 5
+// min = 1
+
+var words = ["he", "yeah", "come", "hello", "yes!"]
+// var randomize = Math.floor(Math.random() * words.length);
+
+// var box = document.getElementById("quote-box")
 
  function generateQuotes() {
-  var randomQuotes = quotes[Math.floor(Math.random()*quotes.length)];
-  console.log(randomQuotes);
+  // var randomKey = Math.floor(Math.random() * (max - min) + min)
+  // var genKey = key+randomKey
+
+  // for (var property in quotes) {
+  //   console.log( property + " word is " + quotes[property] )
+  // }
+
+  // var gimme = quotes.key2;
+ //  var randomQuotes = quotes.1;
+  // console.log(quotes[]);
+  // console.log(hello);
+// var randomQuotes = quotes[Math.floor(Math.random()*quotes.length)];
+  // console.log(genKey);
+  // quotes[Math.floor(Math.random()*quotes.length)];
+  var randomize = Math.floor(Math.random() * words.length);
+
+console.log(words[randomize]);
+  // box.outerHTML = randomQuotes;
 }
 
+// box.outerHTML = generateQuotes;
 
 // box.appendChild(generateQuotes);
