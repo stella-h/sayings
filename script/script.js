45499b9c6d1f66599e75e74e9c8aec8ea9f31470
stella-h <stella.halena@gmail.com>
2016-01-24 04:45:47 +1100

    randomize works

--- script/script.js
+++ script/script.js
@@ -1,27 +1,7 @@
-
-var quotes = 
-{ key1:"amazing",
-  key2: "hello", 
-  key3:"crap",
-  key4:"oh man",
-  key5:"hey hey" }
-
-// max = 5
-// min = 1
-
-var words = ["he", "yeah", "come", "hello", "yes!"]
-// var randomize = Math.floor(Math.random() * words.length);
-
+var words = ["amazing", "hello", "crap", "oh man", "hey hey"]
 // var box = document.getElementById("quote-box")
 
  function generateQuotes() {
-  // var randomKey = Math.floor(Math.random() * (max - min) + min)
-  // var genKey = key+randomKey
-
-  // for (var property in quotes) {
-  //   console.log( property + " word is " + quotes[property] )
-  // }
-
   // var gimme = quotes.key2;
  //  var randomQuotes = quotes.1;
   // console.log(quotes[]);
