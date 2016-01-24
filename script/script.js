565da040d135fbfdbf9fc1f1bb532b6b85a93022
stella-h <stella.halena@gmail.com>
2016-01-24 23:46:52 +1100

    words displaying in box, words cache not cleared

--- script/script.js
+++ script/script.js
@@ -1,20 +1,19 @@
-var words = ["amazing", "hello", "crap", "oh man", "hey hey"]
-// var box = document.getElementById("quote-box")
+var words = ["amazing " , "hello " , "crap " , "oh man " , "hey hey ", "chicken ", "crows ", "fish "]
+var box = document.getElementById("quote-box")
 
  function generateQuotes() {
-  // var gimme = quotes.key2;
- //  var randomQuotes = quotes.1;
-  // console.log(quotes[]);
-  // console.log(hello);
-// var randomQuotes = quotes[Math.floor(Math.random()*quotes.length)];
-  // console.log(genKey);
-  // quotes[Math.floor(Math.random()*quotes.length)];
+  // generates the random number from array.
   var randomize = Math.floor(Math.random() * words.length);
 
-console.log(words[randomize]);
-  // box.outerHTML = randomQuotes;
+  var displayWord = document.createTextNode((words[randomize]))
+  box.appendChild(displayWord);
+  // console.log(words[randomize]);
+
+
 }
 
-// box.outerHTML = generateQuotes;
+// clears existing text on div
+// text node with the right text attribute
 
-// box.appendChild(generateQuotes);
+
+
